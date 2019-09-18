ff4e8f8e4a2c5369c8b580dea67d27407a47eea9
Richard Angapin <rangapin@gmx.com>
2019-09-19 00:44:27 +0800

    pulled up API

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,21 @@
+class HomeController < ApplicationController
+  def index
+    require 'net/http'
+    require 'json'
+
+    @url = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @news = JSON.parse(@response)
+
+    @prices_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP&tsyms=USD'
+    @prices_uri = URI(@prices_url)
+    @prices_response = Net::HTTP.get(@prices_uri)
+    @prices = JSON.parse(@prices_response)
+  end
+
+  def prices
+
+  end
+
+end
