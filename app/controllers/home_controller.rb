d2bf73077ac212b921098b7ad7f9a5b820903af3
Richard Angapin <rangapin@gmx.com>
2019-10-03 03:52:47 +0800

    remove search bar & updates coins list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,23 +8,14 @@ class HomeController < ApplicationController
     @response = Net::HTTP.get(@uri)
     @news = JSON.parse(@response)
 
-    @prices_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP,ETH,LTC,EOS,BCH,LINK,BNB,TRX,XLM&tsyms=USD'
+    @prices_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,EOS,LTC,BCH,TRX,MOF,ETC,LINK,&tsyms=USD'
     @prices_uri = URI(@prices_url)
     @prices_response = Net::HTTP.get(@prices_uri)
     @prices = JSON.parse(@prices_response)
   end
 
   def prices
-    require 'net/http'
-    require 'json'
-    if @symbol
-    @symbol = params[:sym]
-    @symbol = @symbol.upcase
 
-    @quote_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + @symbol + '&tsyms=USD'
-    @quote_uri = URI(@quote_url)
-    @quote_response = Net::HTTP.get(@quote_uri)
-    @quote = JSON.parse(@quote_response)
   end
-end
+
 end
